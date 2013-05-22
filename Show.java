352a25640d896a6a4fc03bde82df93946443a4da
Yan Matagne <yan.matagne@gfi.fr>
2013-05-22 21:35:11 +0200

    deux

--- Show.java
+++ Show.java
@@ -10,7 +10,7 @@ public class Show {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		final Text text = new Text("1");
+		final Text text = new Text("2")
 		System.out.println(text.toString());
 	}
 
