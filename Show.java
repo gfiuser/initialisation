a8b0aa7d96d93a66f997505a7932707d2323f8d2
Gregory MASQUELIN <gregory.masquelin@gfi.fr>
2013-08-07 15:41:36 +0200

    modify Show.java# Please enter the commit message for your changes. Lines starting

--- Show.java
+++ Show.java
@@ -10,7 +10,7 @@ public class Show {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		final Text text = new Text("VDU");
+		final Text text = new Text("GMA");
 		System.out.println(text.toString());
 	}
 
